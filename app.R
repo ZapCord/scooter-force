19e0dbfdca061b1f0b774d1b0f2db67f7d28632c
ZapCord <goldcow0309@gmail.com>
2020-12-10 19:26:54 -0700

    initial commit

---
+++ app.R
@@ -0,0 +1,14 @@
+## Tony Johnson
+## Last modified 12/10/2020
+## Pulls heatmaps from kitronyx snowforce3 and a 16x10 pad.
+
+rm(list=ls())
+
+#######################################################################
+## Shiny Gui setup
+#######################################################################
+
+
+ui <- ...
+server <- ...
+shinyApp(ui=ui, server=server)
